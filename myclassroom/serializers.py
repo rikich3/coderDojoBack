66e247a1d579487b794afe86e50639b7ee5a4f69
rikich3 <rchambillap@unsa.edu.pe>
2024-08-30 20:21:41 -0500

    Setting indentation to 2 spaces on myclassroom/serializers.py

--- myclassroom/serializers.py
+++ myclassroom/serializers.py
@@ -3,65 +3,63 @@ from .models import Clase, Entrega, Publicacion, PerfilEstudiante, PerfilDocente
 
 
 class AppUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AppUser
-        fields = ['username', 'password', 'is_estudiante', 'is_docente']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+  class Meta:
+    model = AppUser
+    fields = ['username', 'password', 'is_estudiante', 'is_docente']
+    extra_kwargs = {
+      'password': {'write_only': True}
+    }
 
-    def create(self, validated_data):
-        # Create the AppUser instance
-        user = AppUser.objects.create_user(**validated_data)
-        
-        # Check if the user is a student or a teacher
-        if user.is_estudiante:
-            PerfilEstudiante.objects.create(user=user)
-        if user.is_docente:
-            PerfilDocente.objects.create(user=user)
-
-        return user
-
+  def create(self, validated_data):
+    # Create the AppUser instance
+    user = AppUser.objects.create_user(**validated_data)
+    # Check if the user is a student or a teacher
+    if user.is_estudiante:
+      PerfilEstudiante.objects.create(user=user)
+    if user.is_docente:
+      PerfilDocente.objects.create(user=user)
+    return user
 
 class PublicacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Publicacion
-        fields = ['id', 'content']
+  class Meta:
+    model = Publicacion
+    fields = ['id', 'content']
+        
 class PerfilDocenteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PerfilDocente
-        fields = ['id', 'name']
+  class Meta:
+    model = PerfilDocente
+    fields = ['id', 'name']
 
 class PerfilEstudianteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PerfilEstudiante
-        fields = ['id', 'name']
+  class Meta:
+    model = PerfilEstudiante
+    fields = ['id', 'name']
+        
 class AsignacionSerializer(serializers.ModelSerializer):
   class Meta:
     model = Asignacion
     fields = ["id", "title", "description"]
     
 class RegisterPublicacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Publicacion
-        fields = ['id', 'content', 'clase', 'docente']
+  class Meta:
+    model = Publicacion
+    fields = ['id', 'content', 'clase', 'docente']
 
 class RegisterEntregaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Entrega
-        fields = ['id', 'asignacion', 'estudiante', 'file', 'submitted_at']
+  class Meta:
+    model = Entrega
+    fields = ['id', 'asignacion', 'estudiante', 'file', 'submitted_at']
 
 class RegisterAsignacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Asignacion
-        fields = ['id', 'title', 'description', 'due_date', 'clase']
+  class Meta:
+    model = Asignacion
+    fields = ['id', 'title', 'description', 'due_date', 'clase']
 
 class RegisterClaseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Clase
-        fields = ['id', 'name', 'estudiantes', 'docente']
+  class Meta:
+    model = Clase
+    fields = ['id', 'name', 'estudiantes', 'docente']
 
-    
 class EntregaSerializerAlDocente(serializers.ModelSerializer):
   estudiante = PerfilEstudianteSerializer()
   asignacion = AsignacionSerializer()
@@ -76,10 +74,9 @@ class ClaseSerializerAlDocente(serializers.ModelSerializer):
   publicaciones = PublicacionSerializer(many=True, read_only=True)
   estudiantes = PerfilEstudianteSerializer(many=True, read_only=True)
 
-  
   class Meta:
-      model = Clase
-      fields = ['id','docente', 'name', 'entregas', 'publicaciones', 'estudiantes']
+    model = Clase
+    fields = ['id','docente', 'name', 'entregas', 'publicaciones', 'estudiantes']
   def get_entregas(self, obj):
     asignaciones = Asignacion.objects.filter(clase=obj)
     return EntregaSerializerAlDocente(Entrega.objects.filter(asignacion__in=asignaciones), many=True).data
